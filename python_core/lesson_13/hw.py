36ae1eed1831d491e2753a8ac14d9695457104c5
kolya8077 <kolya8077@gmail.com>
2024-02-05 09:43:19 +0200

    html lesson 7

---
+++ python_core/lesson_13/hw.py
@@ -0,0 +1,8 @@
+'''
+Завдання 1
+Напишіть функцію, яка відобразить на екрані такий форматований текст:
+
+"Don't compare yourself with anyone in this world…
+if you do so, you are insulting yourself."
+Bill Gates
+'''
