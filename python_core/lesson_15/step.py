4a1d53abf35984eb2e46b2f20381b9a2b8f9d055
kolya8077 <kolya8077@gmail.com>
2024-02-08 11:06:10 +0200

    Create step.py

---
+++ python_core/lesson_15/step.py
@@ -0,0 +1,23 @@
+def functAVG(*args):
+    print(args)
+    # summa = 0
+    # count = 0
+    # for num in args:
+    #     summa+= num
+    #     count +=1
+    # return summa/count
+# res = functAVG(517,8,9, 36,45,2,14)
+res = functAVG(input().split(' '))
+# print (f"Average = {res}")
+
+customers = ["AdminJane", "adminBob", "STUDENTbob", "studentAlice", "Kate"]
+name = "AdminJane"
+name.find()
+def sayHello(customer):
+    if customer.find("admin") != -1:
+        print(f"Hello dear admin, {customer}")
+    else:
+        print('efewfe')
+
+
+
